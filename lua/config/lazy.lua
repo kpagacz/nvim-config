5a3c5779882e08dad454bb28361bbfdb3d355338
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-08-17 10:03:03 +0200

    updated lazy and neovim to 0.10.1

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.r" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,19 +5,14 @@ return {
       vim.list_extend(opts.ensure_installed, { "stylua", "typescript-language-server" })
     end,
   },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   version = false, -- last release is way too old
-  --   dependencies = {
-  --     "hrsh7th/cmp-nvim-lsp",
-  --     "neovim/nvim-lspconfig",
-  --   },
-  --   config = function()
-  --     local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-  --     capabilities.textDocument.completion.completionItem.snippetSupport = false
-  --     require("lspconfig").rust.setup({
-  --       capabilities = capabilities,
-  --     })
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+    },
+  },
 }
